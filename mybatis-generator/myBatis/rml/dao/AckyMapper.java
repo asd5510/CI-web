d5f6b9e57cc5a3b3efbaff34162eb6377d0d2b97
asd5510 <564550685@qq.com>
2016-07-18 20:10:20 +0800

    Revert "first commit"
    This reverts commit 8b9489f8986a95e584b0304dc35fcfe50dc0413c.

--- mybatis-generator/myBatis/rml/dao/AckyMapper.java
+++
@@ -1,9 +0,0 @@
-package rml.dao;
-
-import rml.model.CI;
-
-public interface CIMapper {
-    int insert(CI record);
-
-    int insertSelective(CI record);
-}
--- src/main/java/rml/service/UserServiceI.java
+++
@@ -1,7 +0,0 @@
-package rml.service;
-
-import rml.model.User;
-
-public interface UserServiceI {
-	boolean login(String name, String password);
-}
